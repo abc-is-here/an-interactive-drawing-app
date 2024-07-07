645681382d854e75fadaee16b9195611ce64905b
abc <abhaygupta.hsj@gmail.com>
2024-07-07 19:42:58 +0530

    I know the project is a bit crippled but it works! I will fix it!

--- script.js
+++ script.js
@@ -2,37 +2,61 @@ const canvas = document.getElementById('drawing-canvas');
 const ctx = canvas.getContext('2d');
 const clearBtn = document.getElementById('clear');
 const colorPicker = document.getElementById('color-picker');
+const fillBtn = document.getElementById('fill');
+const saveBtn = document.getElementById('save');
 
 let drawing = false;
 let color = '#000000';
 
-canvas.addEventListener('mousedown', () => {
-    drawing = true;
-});
-
-canvas.addEventListener('mouseup', () => {
-    drawing = false;
-    ctx.beginPath();
-});
-
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mouseup', stopDrawing);
 canvas.addEventListener('mousemove', draw);
 clearBtn.addEventListener('click', clearCanvas);
 colorPicker.addEventListener('input', (e) => {
     color = e.target.value;
 });
+fillBtn.addEventListener('click', fillCanvas);
+saveBtn.addEventListener('click', saveCanvas);
+
+function startDrawing(e) {
+    drawing = true;
+    draw(e);
+}
+
+function stopDrawing() {
+    drawing = false;
+    ctx.beginPath();
+}
 
 function draw(e) {
     if (!drawing) return;
+
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
     ctx.lineWidth = 5;
     ctx.lineCap = 'round';
     ctx.strokeStyle = color;
 
-    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.moveTo(x, y);
 }
 
 function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function fillCanvas() {
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function saveCanvas() {
+    const link = document.createElement('a');
+    link.download = 'drawing.png';
+    link.href = canvas.toDataURL();
+    link.click();
 }
