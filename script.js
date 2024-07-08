bd075d25327252f5096462337014efb89193d8f5
abc <abhaygupta.hsj@gmail.com>
2024-07-08 07:25:52 +0530

    The project is completed!!

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
 const canvas = document.getElementById('drawing-canvas');
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d', { willReadFrequently: true });
 const clearBtn = document.getElementById('clear');
 const colorPicker = document.getElementById('color-picker');
 const fillBtn = document.getElementById('fill');
+const undoBtn = document.getElementById('undo');
 const saveBtn = document.getElementById('save');
+const title = document.getElementById('title');
 
 let drawing = false;
 let color = '#000000';
+let fillMode = false;
+let canvasHistory = [];
+let undoIndex = -1;
 
-canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousedown', startDrawingOrFilling);
 canvas.addEventListener('mouseup', stopDrawing);
 canvas.addEventListener('mousemove', draw);
 clearBtn.addEventListener('click', clearCanvas);
 colorPicker.addEventListener('input', (e) => {
     color = e.target.value;
+    title.style.color = color;
 });
-fillBtn.addEventListener('click', fillCanvas);
+fillBtn.addEventListener('click', () => {
+    fillMode = !fillMode;
+    fillBtn.textContent = fillMode ? 'Fill Mode On' : 'Fill Mode Off';
+});
+undoBtn.addEventListener('click', undoCanvas);
 saveBtn.addEventListener('click', saveCanvas);
 
-function startDrawing(e) {
-    drawing = true;
-    draw(e);
+function saveState() {
+    if (undoIndex < canvasHistory.length - 1) {
+        canvasHistory = canvasHistory.slice(0, undoIndex + 1);
+    }
+    canvasHistory.push(canvas.toDataURL());
+    undoIndex++;
+}
+
+function startDrawingOrFilling(e) {
+    if (fillMode) {
+        floodFill();
+    } else {
+        drawing = true;
+        draw(e);
+    }
 }
 
 function stopDrawing() {
+    if (drawing) {
+        saveState();
+    }
     drawing = false;
     ctx.beginPath();
 }
@@ -47,11 +72,27 @@ function draw(e) {
 
 function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    saveState();
 }
 
-function fillCanvas() {
+function floodFill() {
     ctx.fillStyle = color;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    saveState();
+}
+
+function undoCanvas() {
+    if (undoIndex <= 0) {
+        clearCanvas();
+        return;
+    }
+    undoIndex--;
+    const canvasPic = new Image();
+    canvasPic.src = canvasHistory[undoIndex];
+    canvasPic.onload = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(canvasPic, 0, 0);
+    };
 }
 
 function saveCanvas() {
@@ -60,3 +101,5 @@ function saveCanvas() {
     link.href = canvas.toDataURL();
     link.click();
 }
+
+saveState();
